c9f0d7befb18b4c07ec26f12bfcfac44a3b38af5
insidemybrain <fellipe.bravo@gmail.com>
2020-05-23 18:06:23 -0300

    [Structure] Added .AAR integration

--- message_module/messengermodule/src/androidTest/java/br/com/pipa/messengemodule/ExampleInstrumentedTest.kt
+++ message_module/messengermodule/src/androidTest/java/br/com/pipa/messengemodule/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.com.pipa.messagemodule", appContext.packageName)
+        assertEquals("br.com.pipa.messengermodule", appContext.packageName)
     }
 }
--- pipa_app/app/src/main/java/br/com/pipa/pipaapp/MainActivity.kt
+++ pipa_app/app/src/main/java/br/com/pipa/pipaapp/MainActivity.kt
@@ -2,11 +2,14 @@ package br.com.pipa.pipaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.pipa.messengemodule.core.MessengerFacade
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val messenger = MessengerFacade()
+        messenger.sendOnWhatsApp(this, "+5511123445567")
     }
 }
